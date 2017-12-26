aeb8887c5c82a396a15bfc87ed0d150e4afa1b5c
lgd <liguodong@zplay.cn>
2017-12-26 18:33:56 +0800

    update to 1.3.0

--- app/src/main/java/com/zplay/playable/playableadsplugin/PlayableAdsAdapter.java
+++ app/src/main/java/com/zplay/playable/playableadsplugin/PlayableAdsAdapter.java
@@ -1,70 +1,97 @@
 package com.zplay.playable.playableadsplugin;
 
 import android.app.Activity;
+import android.util.Log;
 
 import com.playableads.PlayLoadingListener;
 import com.playableads.PlayPreloadingListener;
 import com.playableads.PlayableAds;
 import com.unity3d.player.UnityPlayer;
+
+import java.lang.ref.WeakReference;
+
 /**
  * 2017/9/22.
  */
 public class PlayableAdsAdapter {
+
     private static final String TAG = "PlayableAdsAdapter";
+    private static WeakReference<PlayableAds> instanceRef = null;
 
-    public static void InitPA(final Activity activity, final String appId, final String adUnitId) {
+    public static void InitPA(final Activity activity, final String appId) {
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                PlayableAds.init(activity, appId, adUnitId);
+               instanceRef = new WeakReference<>(PlayableAds.init(activity, appId));
             }
         });
     }
 
-    public static void RequestAd(Activity activity, final String objectName) {
-        activity.runOnUiThread(new Runnable() {
+    public static void RequestAd(String adUnitId, final String objectName) {
+        if (instanceRef == null || instanceRef.get() == null){
+            Log.e(TAG, "RequestAd: PlayableAds instance is null");
+            return;
+        }
+        instanceRef.get().requestPlayableAds(adUnitId, new PlayPreloadingListener() {
             @Override
-            public void run() {
-                PlayableAds.getInstance().requestPlayableAds(new PlayPreloadingListener() {
-                    @Override
-                    public void onLoadFinished() {
-                        UnityPlayer.UnitySendMessage(objectName, "OnLoadFinished", "load finished");
-                    }
+            public void onLoadFinished() {
+                UnityPlayer.UnitySendMessage(objectName, "OnLoadFinished", "load finished");
+            }
 
-                    @Override
-                    public void onLoadFailed(int i, String s) {
-                        UnityPlayer.UnitySendMessage(objectName, "OnLoadFailed", "error code: " + i + "\nmsg: " + s);
-                    }
-                });
+            @Override
+            public void onLoadFailed(int i, String s) {
+                UnityPlayer.UnitySendMessage(objectName, "OnLoadFailed", "error code: " + i + "\nmsg: " + s);
             }
         });
+
     }
 
-    public static void PresentAd(final Activity activity, final String objectName) {
-        if(!PlayableAds.getInstance().canPresentAd()){
+    public static void PresentAd(String adUnitId, final String objectName) {
+        if (instanceRef == null || instanceRef.get() == null){
+            Log.e(TAG, "RequestAd: PlayableAds instance is null");
+            return;
+        }
+
+        PlayableAds ads = instanceRef.get();
+
+        if (!ads.canPresentAd(adUnitId)) {
             UnityPlayer.UnitySendMessage(objectName, "OnPresentError", "cache not finished");
             return;
         }
-        activity.runOnUiThread(new Runnable() {
+        ads.presentPlayableAD(adUnitId, new PlayLoadingListener() {
             @Override
-            public void run() {
-                PlayableAds.getInstance().presentPlayableAD(activity, new PlayLoadingListener() {
-                    @Override
-                    public void playableAdsIncentive() {
-                        UnityPlayer.UnitySendMessage(objectName, "PlayableAdsIncentive", "incentive");
-                    }
+            public void onVideoStart() {
+                UnityPlayer.UnitySendMessage(objectName, "PlayableAdsMessage", "video start");
+            }
 
-                    @Override
-                    public void onAdsError(int i, String s) {
-                        UnityPlayer.UnitySendMessage(objectName, "OnPresentError", "error code: " + i + "\nmsg: " + s);
-                    }
-                });
+            @Override
+            public void onVideoFinished() {
+                UnityPlayer.UnitySendMessage(objectName, "PlayableAdsMessage", "video finished");
+            }
+
+            @Override
+            public void playableAdsIncentive() {
+                UnityPlayer.UnitySendMessage(objectName, "PlayableAdsIncentive", "incentive");
+            }
+
+            @Override
+            public void onAdClosed() {
+                UnityPlayer.UnitySendMessage(objectName, "PlayableAdClosed", "");
+            }
+
+            @Override
+            public void onAdsError(int i, String s) {
+                UnityPlayer.UnitySendMessage(objectName, "OnPresentError", "error code: " + i + "\nmsg: " + s);
             }
         });
     }
 
-    public static boolean canPresentAd(){
-        return PlayableAds.getInstance().canPresentAd();
+    public static boolean canPresentAd(String adUnitId) {
+        if (instanceRef == null || instanceRef.get() == null){
+            Log.e(TAG, "RequestAd: PlayableAds instance is null");
+            return false;
+        }
+        return instanceRef.get().canPresentAd(adUnitId);
     }
 
 }
